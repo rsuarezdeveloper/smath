56cba6c82a33de6b10cbffb88244d6f0c97f01df
Raul Suarez <jrsc22@gmail.com>
2014-08-23 15:51:56 -0500

    Primer Post

---
+++ src/Auth/UserBundle/AuthUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Auth\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AuthUserBundle extends Bundle
+{
+}
---
+++ src/Auth/UserBundle/Entity/User.php
@@ -0,0 +1,64 @@
+<?php
+// src/Acme/UserBundle/Entity/User.php
+
+namespace Auth\UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="Usuario")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    protected $nombre="";
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     * @return User
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string 
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+}
