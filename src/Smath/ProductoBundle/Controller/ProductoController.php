21421eddc00f313a6facd3729815a7d0c11fabc2
Raul Suarez <jrsc22@gmail.com>
2014-09-10 19:31:07 -0500

    Incluyendo jqgrid para listar productos y listas en general

--- src/Smath/ProductoBundle/Controller/ProductoController.php
+++ src/Smath/ProductoBundle/Controller/ProductoController.php
@@ -3,6 +3,7 @@
 namespace Smath\ProductoBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -34,6 +35,95 @@ class ProductoController extends Controller
         return array(
             'entities' => $entities,
         );
+    }
+    /**
+     * Lists all Dos entities.
+     *
+     * @Route("/list", name="producto_list")
+     * @Method("GET")
+     */
+    public function listAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $qb = $em->getRepository('SmathProductoBundle:Producto')->createQueryBuilder('p')
+        	   ->add('select','p.id,p.descripcion, p.estado, p.referencia, p.nombre, l.descripcion linea')
+        	   ->leftJoin('p.linea','l')
+        	   ->orderBy('d.id','DESC');
+        $entities=$qb->getQuery()->getResult();
+		$fields=array(
+			'id'=>'d.id',
+			'descripcion' => 'p.descripcion',
+            'estado'=>'p.estado',
+            'referencia'=>'p.referencia',
+            'nombre'=>'p.nombre',
+            'linea'=>'l.descripcion'
+			);
+
+		///Aplicamos filtros
+	    $request=$this->get('request');
+	    if ( $request->get('_search') && $request->get('_search') == "true" && $request->get('filters') )
+            {
+                    $f=$request->get('filters');
+                    $f=json_decode(str_replace("\\","",$f),true);
+                    $rules=$f['rules'];
+                    foreach($rules as $rule){
+                            $searchField=$fields[$rule['field']];
+                            $searchString=$rule['data'];
+                            if($rule['field']=='fecha'){
+                            $daterange=explode("|", $searchString);
+                            if(count($daterange)==1){
+                            	$dateValue="'".trim(str_replace(" ","",$daterange[0]))."'";
+	                            $qb->andWhere($searchField." =".$dateValue);
+                            }else{
+                            	$minValue="'".trim(str_replace(" ","",$daterange[0]))."'";
+                            	$maxValue="'".trim(str_replace(" ","",$daterange[1]))."'";
+	                            $qb->andWhere($qb->expr()->between($searchField,$minValue , $maxValue));
+                            }
+
+                            }else{
+                                if("null"!=$searchString){
+                                	$qb->andWhere($qb->expr()->like($searchField, $qb->expr()->literal("%".$searchString."%")));
+                                }
+                            }
+                    }
+
+            }
+
+
+	    //Ordenamiento de columnas
+	    //sidx	id
+		//sord	desc
+		$sidx=$this->get('request')->query->get('sidx', 'id');
+		$sord=$this->get('request')->query->get('sord', 'DESC');
+		$qb->orderBy($fields[$sidx],$sord);
+
+
+	    $query=$qb->getQuery()->getResult();
+		$paginator = $this->get('knp_paginator');
+		$pagination = $paginator->paginate(
+		    $query,
+		    $this->get('request')->query->get('page', 1)/*page number*/,
+		   $this->get('request')->query->get('rows', 10)/*limit per page*/
+		);
+        /*return array(
+            'entities' => $entities,
+            'pagination'=>$pagination
+        );*/
+        $response= new Response();
+        $pdata=$pagination->getPaginationData();
+        $r=array();
+        $r['records']=count($query);
+        $r['page']=$this->get('request')->query->get('page', 1);
+        $r['rows']=array();
+        $r['total'] = $pdata['pageCount'];
+
+        foreach($pagination as $row){
+	        $line=$row;
+	      	$r['rows'][]=$line;
+        }
+        $response->setContent(json_encode($r));
+        return $response;
     }
     /**
      * Creates a new Producto entity.
