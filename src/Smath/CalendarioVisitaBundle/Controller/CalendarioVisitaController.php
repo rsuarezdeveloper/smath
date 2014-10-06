937badcbab6baeb618cd73e050cc8f5b98dcd7cb
Raul Suarez <jrsc22@gmail.com>
2014-10-05 20:23:14 -0500

    Adicionando mapa de bogotá

--- src/Smath/CalendarioVisitaBundle/Controller/CalendarioVisitaController.php
+++ src/Smath/CalendarioVisitaBundle/Controller/CalendarioVisitaController.php
@@ -116,6 +116,32 @@ class CalendarioVisitaController extends Controller
     /**
      * Finds and displays a CalendarioVisita entity.
      *
+     * @Route("/gmap", name="calendariovisita_gmap")
+     * @Method("GET")
+     * @Template()
+     */
+    public function gmapAction(){
+        $user = $this->get('security.context')->getToken()->getUser();
+        $empleado = $user->getEmpleado();
+        $hoy = new \DateTime();
+        $em = $this->getDoctrine()->getManager();
+        $qb = $em->getRepository('SmathCalendarioVisitaBundle:CalendarioVisita')->createQueryBuilder('c')
+            ->leftJoin('c.empleado','e')
+            ->leftJoin('c.puntoVenta','pv')
+            ;
+        $qb->Where("c.fechaProgramada LIKE '".$hoy->format('Y-m-d')."%'");
+        if(null!=$empleado){
+            $qb->andWhere('e.id='.$empleado->getId());
+        }
+        //die($qb->getQuery()->getSQL());
+        $citas = $qb->getQuery()->getResult();
+        return array(
+            'citas' => $citas,
+        );
+    }
+    /**
+     * Finds and displays a CalendarioVisita entity.
+     *
      * @Route("/mensaje", name="calendariovisita_mensaje")
      * @Method("GET")
      * @Template()
